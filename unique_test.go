be51ac0256e2859c757729196f6589f37c22bd67
Derek Collison <derek.collison@gmail.com>
2016-02-25 03:10:52 -0800

    fix set test

--- unique_test.go
+++ unique_test.go
@@ -8,11 +8,12 @@ import (
 
 func TestBasicUniqueness(t *testing.T) {
 	n := 10000000
-	m := make(map[string]bool)
+	m := make(map[string]struct{}, n)
 	for i := 0; i < n; i++ {
 		n := Next()
-		if m[n] {
+		if _, ok := m[n]; ok {
 			t.Fatalf("Duplicate NUID found: %v\n", n)
 		}
+		m[n] = struct{}{}
 	}
 }
